37d968a0b067843d3cccbee245e8bb1c0a4c7a47
Audeliano <audeliano@gmail.com>
2015-03-26 15:33:22 -0300

    delet rosaria_original

--- rosaria_original/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/au/catkin_ws/src/rosaria/rosaria
-# Build directory: /home/au/catkin_ws/src/rosaria/rosaria
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
